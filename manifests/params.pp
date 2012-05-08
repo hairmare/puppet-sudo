018cb8f9eb9cca09fd8823c6cbf6c53242f22e8d
Lucas Bickel <hairmare@purplehaze.ch>
2012-05-08 22:09:09 +0300

    i just realized that this was the same in both configs

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,6 +1,6 @@
 class sudo::params {
   case $::operatingsystem {
-    ubuntu, debian: {
+    ubuntu, debian, gentoo: {
       $package = 'sudo'
       $config_file = '/etc/sudoers'
       $config_dir = '/etc/sudoers.d/'
@@ -12,12 +12,6 @@ class sudo::params {
       $config_dir = '/etc/sudoers.d/'
       $source = 'puppet:///modules/sudo/sudoers.rhel'
 	}
-    gentoo: {
-      $package = 'sudo'
-      $config_file = '/etc/sudoers'
-      $config_dir = '/etc/sudoers.d/'
-      $source = 'puppet:///modules/sudo/sudoers.portage'
-    }
     default: {
       fail("Unsupported platform: ${::operatingsystem}")
     }
